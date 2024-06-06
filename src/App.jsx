e018832973cb3a25c5f681eb84c0ddb793d5cfd5
Hassane Kara <focus2022@Hassane-kara.local>
2024-06-06 19:53:07 +0200

    This is my first commit in my project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <h1  className='text-sm'>
+        Hello my project is House Renting
+       </h1>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
